0e9ebf12376dece9aa6be398c04bf0a1af540675
yanshoutong <yanshoutong@sina.cn>
2020-08-22 17:47:36 +0800

    done
    Signed-off-by: yanshoutong <yanshoutong@sina.cn>

--- src/csv_parser.rs
+++ src/csv_parser.rs
@@ -3,19 +3,18 @@ use std::io::{BufReader, Error};
 
 use crate::config::Config;
 
-pub fn read(cfg: &Config) ->Result<Vec<String>, Error> {
-    let input = File::open(&cfg.filename)?;
+pub fn read(cfg: &Config) -> Result<Vec<String>, Error> {
+    let input = File::open(&cfg.filename).unwrap();
     let buffered = BufReader::new(input);
 
-    let mut reader = csv::ReaderBuilder::new()
-        .from_reader(buffered);
+    let mut reader = csv::ReaderBuilder::new().from_reader(buffered);
 
     let mut records: Vec<String> = Vec::new();
     for record in reader.records() {
-        let record = record?;
-        println!("{}", &record[0]);
+        let record = record.unwrap();
+        let record = record.get(cfg.column as usize).unwrap();
+        records.push(String::from(record));
     }
 
     Ok(records)
-
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
-mod csv_parser;
 pub mod config;
+mod csv_parser;
 
 pub fn run(cfg: &config::Config) {
     let results = csv_parser::read(cfg).unwrap();
     for record in results {
-        println!("{}", record);
+        println!(">>> {}", record);
     }
 }
