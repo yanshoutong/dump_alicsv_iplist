139f0ec235ef0445c984813f02203dd5c8e34f18
yanshoutong <yanshoutong@sina.cn>
2020-08-22 18:02:42 +0800

    start to work
    Signed-off-by: yanshoutong <yanshoutong@sina.cn>

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ mod csv_parser;
 
 pub fn run(cfg: &config::Config) {
     let results = csv_parser::read(cfg).unwrap();
-    for record in results {
-        println!(">>> {}", record);
+    for (index, record) in results.iter().enumerate() {
+        println!("{}{}  ansible_host={}  ansible_port=22", cfg.prefix, index, record);
     }
 }
